9ed33f03f9da7376d30da9c0387b684023feb70e
Julien Schueller <schueller@phimeca.com>
2016-04-16 11:02:56 +0200

    Add OTRobOpt.hxx

---
+++ lib/include/otrobopt/OTRobOpt.hxx
@@ -0,0 +1,36 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief The external header file of OTRobOpt
+ *
+ *  Copyright 2005-2016 Airbus-EDF-IMACS-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef OTROBOPT_OTROBOPT_HXX
+#define OTROBOPT_OTROBOPT_HXX
+
+#include "otrobopt/SequentialMonteCarloRobustAlgorithm.hxx"
+#include "otrobopt/MeanMeasure.hxx"
+#include "otrobopt/VarianceMeasure.hxx"
+#include "otrobopt/MeanStandardDeviationTradeoffMeasure.hxx"
+#include "otrobopt/WorstCaseMeasure.hxx"
+#include "otrobopt/JointChanceMeasure.hxx"
+#include "otrobopt/IndividualChanceMeasure.hxx"
+#include "otrobopt/QuantileMeasure.hxx"
+#include "otrobopt/AggregatedMeasure.hxx"
+#include "otrobopt/MeasureFactory.hxx"
+
+#endif /* OTROBOPT_OTROBOPT_HXX */
